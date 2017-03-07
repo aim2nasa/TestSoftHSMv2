c51887381a8dce7d2e8b2d79d48c3d57304b79cc
rossi.kwak <rossi.kwak@gmail.com>
2017-03-07 13:35:15 +0900

    softhsm 헤더 추가

--- showSlotTest/main.cpp
+++ showSlotTest/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include "cryptoki.h"
 
 #define DEFAULT_PKCS11_LIB "softhsm2.dll"
 
@@ -12,6 +13,8 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 	cout << DEFAULT_PKCS11_LIB<<" loaded" << endl;
+
+	CK_C_GetFunctionList pGetFunctionList = NULL;
 
 	FreeLibrary(hDLL);
 	cout << "end of list slot" << endl;
