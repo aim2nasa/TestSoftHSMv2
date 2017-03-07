01000ec2daf0266071a11de20ac154649f00932d
rossi.kwak <rossi.kwak@gmail.com>
2017-03-07 12:28:19 +0900

    softhsm2.dll 라이브러리 로드 구현

--- showSlotTest/main.cpp
+++ showSlotTest/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <windows.h>
+
+#define DEFAULT_PKCS11_LIB "softhsm2.dll"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	HINSTANCE hDLL = LoadLibraryA(DEFAULT_PKCS11_LIB);
+	if (hDLL == NULL) {
+		// Failed to load the PKCS #11 library
+		DWORD dw = GetLastError();
+		cout << "LoadLibraryA failed : " << GetLastError() << endl;
+		return -1;
+	}
+	cout << DEFAULT_PKCS11_LIB<<" loaded" << endl;
+
+	FreeLibrary(hDLL);
 	cout << "end of list slot" << endl;
 	return 0;
 }
