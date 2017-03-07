2b2f3e29b97870438a5f77e6037d9c32ed284c69
rossi.kwak <rossi.kwak@gmail.com>
2017-03-07 15:50:49 +0900

    showSlots 함수 구현중 (slot정보 읽어오는 구현까지)

--- showSlotTest/main.cpp
+++ showSlotTest/main.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+int showSlots(CK_FUNCTION_LIST_PTR p11);
+
 int main(int argc, char* argv[]) {
 	HINSTANCE hDLL = LoadLibraryA(DEFAULT_PKCS11_LIB);
 	if (hDLL == NULL) {
@@ -34,7 +36,46 @@ int main(int argc, char* argv[]) {
 	}
 	cout << "library initialized" << endl;
 
+	showSlots(p11);
+
 	FreeLibrary(hDLL);
 	cout << "end of list slot" << endl;
+	return 0;
+}
+
+int showSlots(CK_FUNCTION_LIST_PTR p11)
+{
+	CK_ULONG ulSlotCount;
+	CK_RV rv = p11->C_GetSlotList(CK_FALSE, NULL_PTR, &ulSlotCount);
+	if (rv != CKR_OK) {
+		cout << "Couldn't get the number of slots" << endl;
+		return -1;
+	}
+	cout << "number of slots:" << ulSlotCount << endl;
+
+	CK_SLOT_ID_PTR pSlotList = (CK_SLOT_ID_PTR)malloc(ulSlotCount*sizeof(CK_SLOT_ID));
+	if (!pSlotList) {
+		cout << "Couldn't allocate memory" << endl;
+		return -1;
+	}
+
+	if (p11->C_GetSlotList(CK_FALSE, pSlotList, &ulSlotCount) != CKR_OK) {
+		cout << "Couldn't get the slot list" << endl;
+		free(pSlotList);
+		return -1;
+	}
+
+	for (CK_ULONG i = 0; i < ulSlotCount; i++) {
+		CK_SLOT_INFO slotInfo;
+		CK_TOKEN_INFO tokenInfo;
+		
+		if (p11->C_GetSlotInfo(pSlotList[i], &slotInfo) != CKR_OK) {
+			cout << "Couldn't get info about slot : 0x" << hex << pSlotList[i] << dec << " (" << pSlotList[i] << ")" << endl;
+			continue;
+		}
+		cout << "Slot : 0x" << hex << pSlotList[i] << dec << " (" << pSlotList[i] << ")" << endl;
+	}
+
+	free(pSlotList);
 	return 0;
 }
