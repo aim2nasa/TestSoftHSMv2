723403ee8d728224e0c2138f1b18e49f45e737f3
rossi.kwak <rossi.kwak@gmail.com>
2017-03-08 12:12:00 +0900

    출력 메세지 일부 수정

--- showSlotTest/main.cpp
+++ showSlotTest/main.cpp
@@ -11,14 +11,14 @@ int showSlots(CK_FUNCTION_LIST_PTR p11);
 int main(int argc, char* argv[]) {
 	HINSTANCE hDLL = LoadLibraryA(DEFAULT_PKCS11_LIB);
 	if (hDLL == NULL) {
-		cout << "LoadLibraryA failed : " << GetLastError() << endl;
+		cout << "ERROR: LoadLibraryA failed : " << GetLastError() << endl;
 		return -1;
 	}
 	cout << DEFAULT_PKCS11_LIB<<" loaded" << endl;
 
 	CK_C_GetFunctionList pGetFunctionList = (CK_C_GetFunctionList)GetProcAddress(hDLL, "C_GetFunctionList");
 	if (pGetFunctionList == NULL) {
-		cout << "getProcAddress failed : " << GetLastError() << endl;
+		cout << "ERROR: getProcAddress failed : " << GetLastError() << endl;
 		FreeLibrary(hDLL);
 		return -1;
 	}
@@ -39,7 +39,7 @@ int main(int argc, char* argv[]) {
 	showSlots(p11);
 
 	FreeLibrary(hDLL);
-	cout << "end of list slot" << endl;
+	cout << "end of show slots" << endl;
 	return 0;
 }
 
@@ -48,19 +48,19 @@ int showSlots(CK_FUNCTION_LIST_PTR p11)
 	CK_ULONG ulSlotCount;
 	CK_RV rv = p11->C_GetSlotList(CK_FALSE, NULL_PTR, &ulSlotCount);
 	if (rv != CKR_OK) {
-		cout << "Couldn't get the number of slots" << endl;
+		cout << "ERROR: Couldn't get the number of slots" << endl;
 		return -1;
 	}
 	cout << "number of slots:" << ulSlotCount << endl;
 
 	CK_SLOT_ID_PTR pSlotList = (CK_SLOT_ID_PTR)malloc(ulSlotCount*sizeof(CK_SLOT_ID));
 	if (!pSlotList) {
-		cout << "Couldn't allocate memory" << endl;
+		cout << "ERROR: Couldn't allocate memory" << endl;
 		return -1;
 	}
 
 	if (p11->C_GetSlotList(CK_FALSE, pSlotList, &ulSlotCount) != CKR_OK) {
-		cout << "Couldn't get the slot list" << endl;
+		cout << "ERROR: Couldn't get the slot list" << endl;
 		free(pSlotList);
 		return -1;
 	}
@@ -69,7 +69,7 @@ int showSlots(CK_FUNCTION_LIST_PTR p11)
 
 		CK_SLOT_INFO slotInfo;
 		if (p11->C_GetSlotInfo(pSlotList[i], &slotInfo) != CKR_OK) {
-			cout << "Couldn't get info about slot : 0x" << hex << pSlotList[i] << dec << " (" << pSlotList[i] << ")" << endl;
+			cout << "ERROR: Couldn't get info about slot : 0x" << hex << pSlotList[i] << dec << " (" << pSlotList[i] << ")" << endl;
 			continue;
 		}
 		cout << "Slot : 0x" << hex << pSlotList[i] << dec << " (" << pSlotList[i] << ")" << endl;
