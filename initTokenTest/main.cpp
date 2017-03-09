0a46b52eda02a1edf22bc9199a136812c09fa471
rossi.kwak <rossi.kwak@gmail.com>
2017-03-09 18:53:08 +0900

    C_InitToken에 대한 기초적인 동작 코드 구현

--- initTokenTest/main.cpp
+++ initTokenTest/main.cpp
@@ -24,5 +24,29 @@ int main(int argc, char* argv[])
 
 int initToken(CK_FUNCTION_LIST_PTR p11)
 {
+	CK_SLOT_ID slotID=2;
+	char label[] = "MyTestLabel1";
+	char soPin[] = "1234";
+
+	CK_RV rv = p11->C_InitToken(slotID, (CK_UTF8CHAR_PTR)soPin, (CK_ULONG)strlen(soPin), (CK_UTF8CHAR_PTR)label);
+	switch (rv)
+	{
+	case CKR_OK:
+		break;
+	case CKR_SLOT_ID_INVALID:
+		cout << "ERROR: CKR_SLOT_ID_INVALID: Slot " << slotID << " does not exist." << endl;
+		return -1;
+	case CKR_PIN_INCORRECT:
+		cout << "ERROR: CKR_PIN_INCORRECT: The given SO PIN does not match the one in the token. Needed when reinitializing the token." << endl;
+		return -1;
+	case CKR_TOKEN_NOT_PRESENT:
+		cout << "ERROR: CKR_TOKEN_NOT_PRESENT: The token is not present. Please read the HSM manual for further assistance." << endl;
+		return -1;
+	default:
+		cout << "ERROR: Could not initialize the token. (" <<hex<< rv << ")" << endl;
+		return -1;
+	}
+	cout << "InitToken OK" << endl;
+
 	return 0;
 }
