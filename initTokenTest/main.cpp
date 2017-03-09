7b3e3eb57a0499fe7c73ba5986d52786f94a3116
rossi.kwak <rossi.kwak@gmail.com>
2017-03-09 18:27:53 +0900

    initTokenTest프로젝트 추가 및 기초 뼈대 작

--- initTokenTest/main.cpp
+++ initTokenTest/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include "library.h"
 
 using namespace std;
 
+int initToken(CK_FUNCTION_LIST_PTR p11);
+
 int main(int argc, char* argv[])
 {
+	void* module;
+	CK_FUNCTION_LIST_PTR p11 = NULL;
+	if (loadLib(&module, &p11) == -1) {
+		cout << "ERROR: loadLib" << endl;
+		return -1;
+	}
+	cout << "loadLib ok" << endl;
+
+	initToken(p11);
+
+	unloadLib(module);
 	cout << "initToken test end" << endl;
+	return 0;
+}
+
+int initToken(CK_FUNCTION_LIST_PTR p11)
+{
 	return 0;
 }
