40d55ccade32a0cf94a85dd711e41b9a64821dcb
rossi.kwak <rossi.kwak@gmail.com>
2017-03-09 17:44:17 +0900

    softhsm2라이브러리를 로드하고 초기화하는 라이브러리를 새로 작성하고 이를 사용하도록 showSlotTest프로젝트를 수정함

--- showSlotTest/main.cpp
+++ showSlotTest/main.cpp
@@ -1,44 +1,22 @@
 #include <iostream>
-#include <windows.h>
-#include "cryptoki.h"
-
-#define DEFAULT_PKCS11_LIB "softhsm2.dll"
+#include "library.h"
 
 using namespace std;
 
 int showSlots(CK_FUNCTION_LIST_PTR p11);
 
 int main(int argc, char* argv[]) {
-	HINSTANCE hDLL = LoadLibraryA(DEFAULT_PKCS11_LIB);
-	if (hDLL == NULL) {
-		cout << "ERROR: LoadLibraryA failed : " << GetLastError() << endl;
-		return -1;
-	}
-	cout << DEFAULT_PKCS11_LIB<<" loaded" << endl;
-
-	CK_C_GetFunctionList pGetFunctionList = (CK_C_GetFunctionList)GetProcAddress(hDLL, "C_GetFunctionList");
-	if (pGetFunctionList == NULL) {
-		cout << "ERROR: getProcAddress failed : " << GetLastError() << endl;
-		FreeLibrary(hDLL);
-		return -1;
-	}
-	cout <<"0x"<<hex<< pGetFunctionList << " C_GetFunctionList retrived" << endl;
-
-	// Load the function list
+	void* module;
 	CK_FUNCTION_LIST_PTR p11 = NULL;
-	(*pGetFunctionList)(&p11);
-
-	// Initialize the library
-	if (p11->C_Initialize(NULL_PTR) != CKR_OK) {
-		cout << "ERROR: Could not initialize the library" << endl;
-		FreeLibrary(hDLL);
+	if (loadLib(&module, &p11) == -1) {
+		cout << "ERROR: loadLib" << endl;
 		return -1;
 	}
-	cout << "library initialized" << endl;
+	cout << "loadLib ok" << endl;
 
 	showSlots(p11);
 
-	FreeLibrary(hDLL);
+	unloadLib(module);
 	cout << "end of show slots" << endl;
 	return 0;
 }
