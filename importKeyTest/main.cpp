8d7925936a79ed0e48fe85b33f328593f2bd7b83
rossi.kwak <rossi.kwak@gmail.com>
2017-03-13 16:14:52 +0900

    initToken의 main에서 openSession 복사해서 적용

--- importKeyTest/main.cpp
+++ importKeyTest/main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+int openSession(CK_FUNCTION_LIST_PTR p11, unsigned long slotID, CK_SESSION_HANDLE *pSession);
+
 int main(int argc, char* argv[])
 {
 	if (argc < 8) {
@@ -18,7 +20,26 @@ int main(int argc, char* argv[])
 	}
 	cout << "loadLib ok" << endl;
 
+	CK_SESSION_HANDLE hSession;
+	if (openSession(p11, atoi(argv[1]), &hSession) == 0) {
+		cout << "openSession OK" << endl;
+	}
+	else{
+		unloadLib(module);
+		return -1;
+	}
+
 	unloadLib(module);
 	cout << "import key test end" << endl;
+	return 0;
+}
+
+int openSession(CK_FUNCTION_LIST_PTR p11, unsigned long slotID, CK_SESSION_HANDLE *pSession)
+{
+	CK_RV rv = p11->C_OpenSession(slotID, CKF_SERIAL_SESSION | CKF_RW_SESSION, NULL_PTR, NULL_PTR, pSession);
+	if (rv != CKR_OK) {
+		cout << "ERROR: Could not open a session with the library." << endl;
+		return -1;
+	}
 	return 0;
 }
