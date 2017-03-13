28725307d62cb43ead66c92c475d0196622609b9
rossi.kwak <rossi.kwak@gmail.com>
2017-03-13 16:18:18 +0900

    initToken의 main에서 login 복사해서 적용

--- importKeyTest/main.cpp
+++ importKeyTest/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int openSession(CK_FUNCTION_LIST_PTR p11, unsigned long slotID, CK_SESSION_HANDLE *pSession);
+int login(CK_FUNCTION_LIST_PTR p11, CK_SESSION_HANDLE *pSession, char* soPin);
 
 int main(int argc, char* argv[])
 {
@@ -23,6 +24,14 @@ int main(int argc, char* argv[])
 	CK_SESSION_HANDLE hSession;
 	if (openSession(p11, atoi(argv[1]), &hSession) == 0) {
 		cout << "openSession OK" << endl;
+
+		if (login(p11, &hSession, argv[2]) == 0) {
+			cout << "login OK" << endl;
+		}
+		else{
+			unloadLib(module);
+			return -1;
+		}
 	}
 	else{
 		unloadLib(module);
@@ -39,6 +48,16 @@ int openSession(CK_FUNCTION_LIST_PTR p11, unsigned long slotID, CK_SESSION_HANDL
 	CK_RV rv = p11->C_OpenSession(slotID, CKF_SERIAL_SESSION | CKF_RW_SESSION, NULL_PTR, NULL_PTR, pSession);
 	if (rv != CKR_OK) {
 		cout << "ERROR: Could not open a session with the library." << endl;
+		return -1;
+	}
+	return 0;
+}
+
+int login(CK_FUNCTION_LIST_PTR p11, CK_SESSION_HANDLE *pSession, char* soPin)
+{
+	CK_RV rv = p11->C_Login(*pSession, CKU_SO, (CK_UTF8CHAR_PTR)soPin, (CK_ULONG)strlen(soPin));
+	if (rv != CKR_OK) {
+		cout << "ERROR: Could not log in on the token. (0x" << hex << rv << ")" << endl;
 		return -1;
 	}
 	return 0;
