6771a79907685ebd73a90266e725088236f6888f
rossi.kwak <rossi.kwak@gmail.com>
2017-03-15 12:39:53 +0900

    usage 추가

--- deleteKeyTest/main.cpp
+++ deleteKeyTest/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+	if (argc < 3) {
+		cout << "usage: deleteKeyTest <serial> <label>" << endl;
+		return -1;
+	}
+
 	cout << "delete key test end" << endl;
 	return 0;
 }
