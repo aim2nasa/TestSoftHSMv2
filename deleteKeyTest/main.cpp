8e0efe31ac612809df88b8d65caa4288fb13441e
rossi.kwak <rossi.kwak@gmail.com>
2017-03-15 12:48:26 +0900

    serial, token입력 값 오류 체크

--- deleteKeyTest/main.cpp
+++ deleteKeyTest/main.cpp
@@ -9,6 +9,15 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
+	char* serial = argv[1];
+	char* token = argv[2];
+
+	if (serial == NULL && token == NULL) {
+		fprintf(stderr, "ERROR: A token must be supplied. "
+			"Use --serial <serial> or --token <label>\n");
+		return -1;
+	}
+
 	cout << "delete key test end" << endl;
 	return 0;
 }
