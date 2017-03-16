2cd8b990e2859d1a198b5c4af976c1bb66fc92bd
rossi.kwak <rossi.kwak@gmail.com>
2017-03-16 15:03:18 +0900

    convarch.lib의 MutexFactory코드를 포팅하고 프로젝트를 설정함 (include,lib)

--- deleteKeyTest/helper.cpp
+++ deleteKeyTest/helper.cpp
@@ -1,7 +1,20 @@
 #include "helper.h"
+#include "common\MutexFactory.h"
+
+#ifdef HAVE_CXX11
+
+std::unique_ptr<MutexFactory> MutexFactory::instance(nullptr);
+
+#else
+
+std::auto_ptr<MutexFactory> MutexFactory::instance(NULL);
+
+#endif
 
 bool initSoftHSM()
 {
+	// Not using threading
+	MutexFactory::i()->disable();
 	return true;
 }
 
