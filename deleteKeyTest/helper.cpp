17c7d482092816611ae155e90d75d44a7b7fe910
rossi.kwak <rossi.kwak@gmail.com>
2017-03-16 15:27:03 +0900

    SecureMemoryRegistry관련 코드 적용에 따른 헤더 포함, 프로젝트 설정 포�

--- deleteKeyTest/helper.cpp
+++ deleteKeyTest/helper.cpp
@@ -1,13 +1,16 @@
 #include "helper.h"
 #include "MutexFactory.h"
+#include "SecureMemoryRegistry.h"
 
 #ifdef HAVE_CXX11
 
 std::unique_ptr<MutexFactory> MutexFactory::instance(nullptr);
+std::unique_ptr<SecureMemoryRegistry> SecureMemoryRegistry::instance(nullptr);
 
 #else
 
 std::auto_ptr<MutexFactory> MutexFactory::instance(NULL);
+std::auto_ptr<SecureMemoryRegistry> SecureMemoryRegistry::instance(NULL);
 
 #endif
 
@@ -15,6 +18,14 @@ bool initSoftHSM()
 {
 	// Not using threading
 	MutexFactory::i()->disable();
+
+	// Initiate SecureMemoryRegistry
+	if (SecureMemoryRegistry::i() == NULL)
+	{
+		fprintf(stderr, "ERROR: Could not initiate SecureMemoryRegistry.\n");
+		return false;
+	}
+
 	return true;
 }
 
