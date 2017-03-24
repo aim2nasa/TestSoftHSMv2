0eb9ebf55bd63e1ff77501cb80f2b49513db3bf4
rossi.kwak <rossi.kwak@gmail.com>
2017-03-24 17:13:41 +0900

    OpenSession구현
    슬롯이 아래와 같다고 가정하고 slotID값을 하드코딩함
    
    ----------------------------------------------
    C:\SoftHSM2\bin>softhsm2-util.exe --show-slots
    Available slots:
    Slot 1868647305
        Slot info:
            Description:      SoftHSM slot ID 0x6f614b89
            Manufacturer ID:  SoftHSM project
            Hardware version: 2.2
            Firmware version: 2.2
            Token present:    yes
        Token info:
            Manufacturer ID:  SoftHSM project
            Model:            SoftHSM v2
            Hardware version: 2.2
            Firmware version: 2.2
            Serial number:    eecc022d6f614b89
            Initialized:      yes
            User PIN init.:   yes
            Label:            MyToken 1
    Slot 1
        Slot info:
            Description:      SoftHSM slot ID 0x1
            Manufacturer ID:  SoftHSM project
            Hardware version: 2.2
            Firmware version: 2.2
            Token present:    yes
        Token info:
            Manufacturer ID:  SoftHSM project
            Model:            SoftHSM v2
            Hardware version: 2.2
            Firmware version: 2.2
            Serial number:
            Initialized:      no
            User PIN init.:   no
            Label:
    
    C:\SoftHSM2\bin>

--- privObjTest/main.cpp
+++ privObjTest/main.cpp
@@ -12,6 +12,18 @@ int main(int argc, char* argv[]) {
 	}
 	cout << "loadLib ok" << endl;
 
+	unsigned long slotID = 1868647305;	//하드코딩 변수임: softhsm2-util.exe --show-slots로 조회된 슬롯 아이디를 입력해야 함
+
+	CK_SESSION_HANDLE hSession;
+	CK_RV rv = p11->C_OpenSession(slotID, CKF_SERIAL_SESSION | CKF_RW_SESSION, NULL_PTR, NULL_PTR, &hSession);
+	if (rv == CKR_OK) {
+		cout <<hex<< "openSession OK: 0x" << (unsigned long)rv << endl;
+	}
+	else{
+		cout <<hex<< "openSession failed: 0x" << (unsigned long)rv << endl;
+		unloadLib(module);
+		return -1;
+	}
 
 	unloadLib(module);
 	cout << "private object test end" << endl;
