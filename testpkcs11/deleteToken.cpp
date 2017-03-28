50a151771131673d9e22c4e7491fac03fb59bfb6
rossi.kwak <rossi.kwak@gmail.com>
2017-03-28 17:24:45 +0900

    deleteToken 구현

--- testpkcs11/deleteToken.cpp
+++ testpkcs11/deleteToken.cpp
@@ -1,6 +1,38 @@
 #include "deleteToken.h"
 
-void deleteToken(char *serial, char* token)
-{
+class CSoftHsm{
+public:
+	CSoftHsm(){ initSoftHSM(); }
+	~CSoftHsm(){ finalizeSoftHSM(); }
+};
 
+int deleteToken(char *serial, char* token)
+{
+	CSoftHsm sHsm;	//스택에 두어 자동으로 생성자와 소멸자가 호출되도록
+
+	std::string basedir = Configuration::i()->getString("directories.tokendir", DEFAULT_TOKENDIR);
+	std::string tokendir;
+
+	if (findTokenDirectory(basedir, tokendir, serial, token))
+	{
+		std::string fulldir = basedir;
+		if (fulldir.find_last_of(OS_PATHSEP) != (fulldir.size() - 1))
+		{
+			fulldir += OS_PATHSEP + tokendir;
+		}
+		else
+		{
+			fulldir += tokendir;
+		}
+
+		if (rmdir(fulldir))
+		{
+			printf("The token (%s) has been deleted.\n", fulldir.c_str());
+			return 0;
+		}
+		else{
+			return -2;
+		}
+	}
+	return -1;
 }
--- testpkcs11/deleteToken.h
+++ testpkcs11/deleteToken.h
@@ -1,1 +1,5 @@
-void deleteToken(char *serial, char* token);
+#include "helper.h"
+#include "Configuration.h"
+#include "OSPathSep.h"
+
+int deleteToken(char *serial, char* token);
