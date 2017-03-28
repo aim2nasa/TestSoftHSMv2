001e3bd794d4df6008d4b579fca248293737a7b6
rossi.kwak <rossi.kwak@gmail.com>
2017-03-28 12:34:02 +0900

    soPin,label을 명령줄에서 읽어 들여 토큰을 초기화 하는 코드 구현

--- testpkcs11/main.cpp
+++ testpkcs11/main.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	if (argc < 3) {
+		cout << "usage: testpkcs11 <soPin> <label>" << endl;
+		return -1;
+	}
+
 	void* module;
 	CK_FUNCTION_LIST_PTR p11 = NULL;
 	if (loadLib(&module, &p11) == -1) {
@@ -22,6 +27,14 @@ int main(int argc, char* argv[]) {
 
 	CK_SLOT_ID slotID = ulSlotCount - 1; //디폴트로 들어가는 한개의 카운트를 제외한다. 슬롯이 하나도 없을때도 카운트는 1로 나오므로
 	cout << "SlotID : 0x" << hex << slotID << dec << " (" << slotID << ")" << endl;
+
+	char *soPin = argv[1];
+	char *label = argv[2];
+	rv = p11->C_InitToken(slotID, (CK_UTF8CHAR_PTR)soPin, (CK_ULONG)strlen(soPin), (CK_UTF8CHAR_PTR)label);
+	if (rv != CKR_OK) {
+		cout << "ERROR: C_InitToken: 0x" << hex << rv << endl;
+		return -1;
+	}
 
 	unloadLib(module);
 	cout << "end of testpkcs11" << endl;
