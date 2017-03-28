4c5cfb6f94699d5c3fee582370024c3eb03044db
rossi.kwak <rossi.kwak@gmail.com>
2017-03-28 12:25:01 +0900

    softhsm은 슬롯을 카운트하면 디폴트로 들어가는 하나의 실제 슬롯이 아닌(?)슬롯을 포함해서 카운팅을 한다. 슬롯을 생성할때는 이 카운트보다는 하나 작은 슬롯아이디를 지정해서 토큰을 생성해야 한다. 이러한 로직을 반영하여
    슬롯갯수를 세고 이를 이용하여 새로 생성하고자 하는 토큰의 슬롯아이디를 지정하는 로직을 구현

--- testpkcs11/main.cpp
+++ testpkcs11/main.cpp
@@ -12,6 +12,17 @@ int main(int argc, char* argv[]) {
 	}
 	cout << "loadLib ok" << endl;
 
+	CK_ULONG ulSlotCount;
+	CK_RV rv = p11->C_GetSlotList(CK_FALSE, NULL_PTR, &ulSlotCount);
+	if (rv != CKR_OK) {
+		cout << "ERROR: Couldn't get the number of slots: 0x" << hex << rv << endl;
+		return -1;
+	}
+	cout << "number of slots:" << ulSlotCount << endl;
+
+	CK_SLOT_ID slotID = ulSlotCount - 1; //디폴트로 들어가는 한개의 카운트를 제외한다. 슬롯이 하나도 없을때도 카운트는 1로 나오므로
+	cout << "SlotID : 0x" << hex << slotID << dec << " (" << slotID << ")" << endl;
+
 	unloadLib(module);
 	cout << "end of testpkcs11" << endl;
 	return 0;
