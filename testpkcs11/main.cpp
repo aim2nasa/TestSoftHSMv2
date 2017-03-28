8b061e7e19c440d26e236e63c01a8473eaf6f2ec
rossi.kwak <rossi.kwak@gmail.com>
2017-03-28 11:49:54 +0900

    softhsm2라이브러리 링크, 라이브러리 로드 완료

--- testpkcs11/main.cpp
+++ testpkcs11/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "library.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	void* module;
+	CK_FUNCTION_LIST_PTR p11 = NULL;
+	if (loadLib(&module, &p11) == -1) {
+		cout << "ERROR: loadLib" << endl;
+		return -1;
+	}
+	cout << "loadLib ok" << endl;
+
+	unloadLib(module);
 	cout << "end of testpkcs11" << endl;
 	return 0;
 }
