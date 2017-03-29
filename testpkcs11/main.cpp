4a9dbb8ddbb2cdd7c0a9cb0d5528089e4ce9c7e0
rossi.kwak <rossi.kwak@gmail.com>
2017-03-29 13:49:39 +0900

    createToken,deleteToken리턴값에 따른 오류 메세지 출력 추가 토큰 생성 및 삭제 메세지 출력

--- testpkcs11/main.cpp
+++ testpkcs11/main.cpp
@@ -23,9 +23,18 @@ int main(int argc, char* argv[]) {
 	char *label = argv[2];
 	char *userPin = argv[3];
 
-	createToken(p11,soPin,label,userPin);
+	int nRtn = 0;;
+	if ( (nRtn = createToken(p11, soPin, label, userPin)) != 0) {
+		cout << "ERROR: createToken(" <<soPin<<","<<label<<","<<userPin<<")="<<nRtn<< endl;
+		return -1;
+	}
+	cout << "token("<<label<<") created" << endl;
 
-	deleteToken(NULL_PTR, label);
+	if ( (nRtn=deleteToken(NULL_PTR, label)) != 0) {
+		cout << "ERROR: deleteToken(," << label << ")=" << nRtn << endl;
+		return -1;
+	}
+	cout << "token(" << label << ") deleted" << endl;
 
 	unloadLib(module);
 	cout << "end of testpkcs11" << endl;
